1a2808785b1bf5718c24ee55fc84848318a7c88d
nams1570 <amanganapathy@gmail.com>
2024-02-17 01:39:55 -0800

    Added capability for projects and milestones to be instantiated from dictionaries, and created server to handle post requests with project data in jsons.

--- Project.py
+++ Project.py
@@ -8,19 +8,15 @@ class MilestoneConfig:
         self.funds = funds
         self.description = description
 
-class Project:
-    def __init__(self,name, allConfigs: list[MilestoneConfig]):
-        if len(allConfigs) == 0:
-            raise Exception("must have at least one milestone")
-        self.name = name
-        self.milestones = []
-        self.isFulfilled = False
-        for config in allConfigs:
-            self.milestones.append(Milestone(config))
-    
-    def markFulfilled(self):
-        self.isFulfilled = True
-        
+    def __init__(self,config:dict):
+        if "name" not in config or "time" not in config or "funds" not in config or "description" not in config:
+            raise Exception("Incorrect format for a config")
+        self.name,self.time,self.funds,self.description = config['name'],config['time'],config['funds'],config['description']
+
+    def __str__(self):
+        result = f"name: {self.name}, time: {self.time}, funds: {self.funds}, description: {self.description}"
+        return result
+
 class Milestone:
     def __init__(self,config: MilestoneConfig):
         self.config = config
@@ -31,5 +27,40 @@ class Milestone:
 
     def markFulfilled(self):
         self.isFulfilled = True
+    
+    def __str__(self):
+        result = f"{self.config}, isFulfilled: {self.isFulfilled}"
+        return result
+
+
+class Project:
+    def __init__(self,name, allConfigs: list[MilestoneConfig]):
+        if len(allConfigs) == 0:
+            raise Exception("must have at least one milestone")
+        self.name = name
+        self.milestones = []
+        self.isFulfilled = False
+        for config in allConfigs:
+            self.milestones.append(Milestone(config))
+    
+    def __init__(self,project:dict):
+        if "name" not in project or "allConfigs" not in project: 
+            raise Exception("Incorrect format for a Project")
+        self.name,self.isFulfilled = project['name'],False
+        self.milestones = []
+        for milestone in project['allConfigs']:
+            config = MilestoneConfig(milestone)
+            self.milestones.append(Milestone(config))
+
+    def __str__(self):
+        result = f"name: {self.name}, isFulfilled: {self.isFulfilled}, milestones: [ "
+        for milestone in self.milestones:
+            result += "{" +f" {milestone} " + "},"
+        result += " ]"
+        return result
+
+    def markFulfilled(self):
+        self.isFulfilled = True
+        
         
     
